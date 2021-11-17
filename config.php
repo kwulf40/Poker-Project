2a1dada4683f2f244f5bf16b3223c483a0256f27
kwulf40 <kwulf40@gmail.com>
2021-11-17 10:22:57 -0800

    Minor Site input and visual updates to prepare for PHP/SQL link

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/*Database credentials.*/
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'group2');
+define('DB_PASSWORD', 'Lb4iTSy5adds');
+define('DB_NAME', 'group2');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("<p>Unable to connect to database server.</p> " . "<p>Database Error Code " . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p>");
+}
+?>
