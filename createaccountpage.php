7c58c52d0b5f8d13abdaccac96fcb696e6b231b6
kwulf40 <kwulf40@gmail.com>
2021-11-28 22:05:21 -0800

    Remove js

--- createaccountpage.php
+++ createaccountpage.php
@@ -121,7 +121,6 @@
         <meta charset = "utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href = "createaccountpage.css"/>
-        <!--script src="createaccountpage.js"></script-->
     </head>
     <body>
         <div class="bg">
